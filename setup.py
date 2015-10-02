b0197bd13a3ed268f56e98bbe5c370dd988f237d
siboles <scott.sibole@gmail.com>
2015-10-02 13:52:11 -0600

    Updated version to correct URL to homepage.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from distutils.core import setup
 
 setup(
     name = 'febio',
-    version = '0.1',
+    version = '0.1.1',
     packages = ['febio',],
     py_modules = ['febio.__init__','febio.MatDef','febio.MeshDef','febio.Boundary','febio.Control','febio.Load','febio.Model'],
     author = 'Scott Sibole',
     author_email = 'ssibole@kin.ucalgary.ca',
     license = 'MIT',
     package_data = {'febio': ['verification/*'],},
-    url = 'https:/github.com/siboles/pyFEBio',
-    download_url = 'https://github.com/siboles/pyFEBio/tarball/0.1',
+    url = 'https://github.com/siboles/pyFEBio',
+    download_url = 'https://github.com/siboles/pyFEBio/tarball/0.1.1',
     description = 'A Python API for FEBio',
 )
